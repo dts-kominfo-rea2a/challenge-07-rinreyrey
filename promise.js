c4c0d4bbf04e2496177165cac6242a8e377e08cd
Rindang Reynaldi <rindangr79@gmail.com>
2022-06-15 19:46:23 +0700

    challenge-7-rindang

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (kemarahan) => {
+  return new Promise((resolve) => {
+    const arrayTotal = [];
+    promiseTheaterIXX().then((data) =>
+      data.forEach((d) => arrayTotal.push(d.hasil))
+    );
+    promiseTheaterVGC().then((data) => {
+      data.forEach((d) => arrayTotal.push(d.hasil));
+      const arrayFiltered = arrayTotal.filter((d) => d == kemarahan);
+      return arrayFiltered.length;
+    }).then((data) => resolve(data));
+
+
+  })
+
+};
 
 module.exports = {
   promiseOutput,
